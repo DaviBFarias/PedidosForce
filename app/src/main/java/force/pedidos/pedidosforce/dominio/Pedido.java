896c84d8c062395fd4fe46e69744cbc4df829d21
Alyson <alyson.phone@gmail.com>
2018-11-12 09:27:04 -0300

    Atualização de tela de pedidos - Alyson

--- app/src/main/java/force/pedidos/pedidosforce/dominio/Pedido.java
+++ app/src/main/java/force/pedidos/pedidosforce/dominio/Pedido.java
@@ -1,77 +1,21 @@
 package force.pedidos.pedidosforce.dominio;
 
-import android.text.Editable;
-
 import java.util.ArrayList;
 
 public class Pedido {
 
+
+     private Cliente cliente;
      private int codigoPagamento;
-    // private Cliente cliente;
-    private String cliente;
-     //private TabelaPreco tabelaPreco;
-     private String tabelaPreco;
-     private String pagamento, obs;
+     private ArrayList<Produto> arrayProduto = new ArrayList<Produto>();
 
-     public Pedido(int codigoPagamento, String cliente, String tabelaPreco, String pagamento, String obs){
-          this.codigoPagamento = codigoPagamento;
-          this.cliente = cliente;
-          this.tabelaPreco = tabelaPreco;
-          this.pagamento = pagamento;
-          this.obs = obs;
-
+     public ArrayList<Produto> getArrayProduto() {
+          return arrayProduto;
      }
 
-
-    public int getCodigoPagamento() {
-        return codigoPagamento;
-    }
-
-    public void setCodigoPagamento(int codigoPagamento) {
-        this.codigoPagamento = codigoPagamento;
-    }
-
-    public String getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(String cliente) {
-        this.cliente = cliente;
-    }
-
-    public String getTabelaPreco() {
-        return tabelaPreco;
-    }
-
-    public void setTabelaPreco(String tabelaPreco) {
-        this.tabelaPreco = tabelaPreco;
-    }
-
-    public String getPagamento() {
-        return pagamento;
-    }
-
-    public void setPagamento(String pagamento) {
-        this.pagamento = pagamento;
-    }
-
-    public String getObs() {
-        return obs;
-    }
-
-    public void setObs(String obs) {
-        this.obs = obs;
-    }
-
-    public String cadastraPedido(){
-
-
-
-     return cliente + " " + tabelaPreco + " " + pagamento + " " +obs;
-
+     public void setArrayProduto(ArrayList<Produto> arrayProduto) {
+          this.arrayProduto = arrayProduto;
      }
-
-
 
 
 
