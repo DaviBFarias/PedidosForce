a3757fdbb6a8535e8ea504afd7bb877a89848248
AndreilsonMoreira <andreilson.moreira@gmail.com>
2018-11-12 09:24:25 -0300

    Criação de layout da tela inicial com CardView.

--- app/src/main/java/force/pedidos/pedidosforce/MenuActivity.java
+++ app/src/main/java/force/pedidos/pedidosforce/MenuActivity.java
@@ -1,36 +1,42 @@
 package force.pedidos.pedidosforce;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener{
 
+    CardView card;
+
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
 
+     card =(CardView) findViewById(R.id.addpedido);
 
-        // ativar botao que chama a class autentica.
-        Button botao = (Button) findViewById(R.id.pedido);
-        botao.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+       card.setOnClickListener(this);
 
-                Intent digitarPedido = new Intent(v.getContext(), PedidosActivity.class);
-                startActivity(digitarPedido);
-
-            }
-        });
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()){
 
+            case R.id.addpedido : i = new Intent(this, PedidosActivity.class); startActivity(i); break;
 
+            default:Toast.makeText(this, "Entendeu Andreilson?", Toast.LENGTH_SHORT).show(); break;
 
+        }
 
-
-
+    }
 }
